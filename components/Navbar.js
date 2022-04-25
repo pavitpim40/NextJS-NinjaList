f1a91f86048a1b65140b786d2930e8f6384fdb51
Pavit <pavitpim@gmail.com>
2022-04-25 15:24:38 +0700

    Layout Entire Web

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 const Navbar = () => {
   return (
     <nav>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
-
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
-     <h1>Homepage</h1>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper    </p>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper    </p>
+      <h1>Homepage</h1>
+      <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+        egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
+        leo. Quisque sit amet est et sapien ullamcorper{" "}
+      </p>
+      <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+        egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
+        leo. Quisque sit amet est et sapien ullamcorper{" "}
+      </p>
       <Link href="/ninjas">
         <a>
           <h2>See Ninja Listing</h2>
         </a>
       </Link>
-      <Footer/>
     </div>
-  )
+  );
 }
