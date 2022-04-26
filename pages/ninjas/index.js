6d29483b6d63bbfceb350ca06016867e5bbf414b
Pavit <pavitpim@gmail.com>
2022-04-26 11:55:32 +0700

    dynamics route

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import styles from '../../styles/Ninjas.module.css';
-
+import Ling from "next/link"
+import Link from 'next/link';
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
@@ -13,11 +14,11 @@ const Ninjas = ({ninjas}) => {
     return ( <div>
         <h1>Ninja</h1>
         {ninjas.map(ninja => (
-            <div key={ninja.id}>
+            <Link href={"/ninjas/" + ninja.id }key={ninja.id}>
                 <a className={styles.single}>
                     <h3>{ ninja.name }</h3>
                 </a>
-                </div>
+                </Link>
         ))}
     </div> );
 }
